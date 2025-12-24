e392166a7e2ec6bc9a0e59a8134c866cad799270
Fernando-Lima <fernandosupweb@gmail.com>
2025-12-24 09:18:59 -0300

    initial commit

---
+++ src/test/java/com/portella/teste/portella/PortellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portella.teste.portella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
